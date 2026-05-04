0d5ea74b72f396804f473810555e63dee4ec7112
Romain Grecourt <romain.grecourt@oracle.com>
2026-05-04 14:12:53 -0700

    Remove @Validated from declarative HTTP endpoint Add Service.Singleton to pass validation codegen Update FeaturesGenerationIT to pass with the removal of Validated.

--- extensions/openapi-generator/modules/openapi-generator/src/test/java/io/helidon/openapi/generator/FeaturesGenerationIT.java
+++ extensions/openapi-generator/modules/openapi-generator/src/test/java/io/helidon/openapi/generator/FeaturesGenerationIT.java
@@ -200,12 +200,6 @@ class FeaturesGenerationIT {
     }
 
     @Test
-    void apiInterfaceHasValidatedAnnotationWhenParamValidationIsPresent() throws IOException {
-        assertThat(read(apiFile("ThingsApi.java")),
-                   containsString("@Validation.Validated"));
-    }
-
-    @Test
     void apiInterfaceIntParamHasExclusiveMinMaxValidation() throws IOException {
         String content = read(apiFile("ThingsApi.java"));
         assertThat(content, containsString("@Validation.Integer.Min(11)"));
